5172aeb8178396494d7def367d61f977b8566032
Vladimir Efanov <vuefanov@yandex.ru>
2023-06-11 07:13:19 +0300

    first commit

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+public class Main {
+    public static void main(String[] args) {
+
+        int price = 13676;
+        int miles = price / 20;
+
+        System.out.println("Начислено миль: " + miles);
+
+
+
+    }
+
+
+}
